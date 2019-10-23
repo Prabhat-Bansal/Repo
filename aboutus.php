548450be24ed59ba2d36a7e8296e01d32e69f786
Prabhat-Bansal <48401361+Prabhat-Bansal@users.noreply.github.com>
2019-10-23 10:29:13 +0530

    First Commit

---
+++ aboutus.php
@@ -0,0 +1,6 @@
+<h2 style="background:#99FF33;padding:5px" align="left">About us</h2>
+<p> 
+</p>
+
+<p>
+</p>
---
+++ contactus.php
@@ -0,0 +1,30 @@
+<style>
+	table{padding:5px;border-radius:5px}
+	td{padding:10px}
+</style>
+<table width="90%" border="1" align="center">
+  <tr style="background:#CCCCCC" height="30px">
+    <th colspan="2" style="color:#0000FF">Contact Us</th>
+  </tr>
+  <tr>
+    <td width="121" height="37">Name</td>
+    <td width="252">Group G5 </td>
+  </tr>
+  <tr>
+    <td height="36">Mobile </td>
+    <td> 9899933365 </td>
+  </tr>
+  <tr>
+    <td>Add</td>
+    <td>Seelampur</td>
+  </tr>
+    <tr>
+    <td>Email</td>
+    <td>group523@gmail.com</td>
+  </tr>
+   <tr>
+    <td>Website</td>
+    <td><a href="http://www.phptpoint.com">sjdkh</a></td>
+  </tr>
+  
+</table>
